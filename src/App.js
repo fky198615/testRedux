edeee92562659cefc6ef213924bf442fa625d5c9
RomSham <80100691+RomSham@users.noreply.github.com>
2021-04-22 18:48:04 +0200

    v-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Header from "./component/header";
+import './App.css'
+
+import HomePage from "./pages/homePage";
+import Register from "./component/register";
+import SignIn from "./component/signIn";
+import CategoryPage from "./pages/categoryPage";
+import Category1Page from "./pages/category1Page";
+import Category2Page from "./pages/category2Page";
+import Category3Page from "./pages/category3Page";
+import Footer from "./component/footer";
+
+import {ProductPage} from "./pages/productPage";
+import {Cart} from "./component/cart";
+import {Product} from "./component/product";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+
+        <Router>
+                <Header/>
+            <Switch>
+                <Route path='/' exact component={HomePage}/>
+                <Route path='/register' exact component={Register}/>
+                <Route path='/signIn' exact component={SignIn}/>
+
+                    <Route path='/categoryPage' exact component={CategoryPage}/>
+                    <Route path='/category1Page' exact component={Category1Page}/>
+                    <Route path='/category2Page' exact component={Category2Page}/>
+                    <Route path='/category3Page' exact component={Category3Page}/>
+                <Route path='/productPage' component={ProductPage} />
+
+                <Route path='/cart' component={Cart}/>
+                <Router path='/product/:id' component={Product}/>
+            </Switch>
+
+            <Footer />
+        </Router>
+
+    );
 }
 
 export default App;
